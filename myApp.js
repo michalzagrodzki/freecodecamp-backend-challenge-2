3512c60e22c94f355d3855229b56d9c204dcc273
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-20 13:55:01 +0200

    Exercise 5.

--- myApp.js
+++ myApp.js
@@ -7,4 +7,8 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
+});
+
 module.exports = app;
