c7e77ae75bbf26a8582d15d90ba59427bc508624
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-20 14:16:46 +0200

    Exercise 7.

--- myApp.js
+++ myApp.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use((req, res, next) => {
+  const method = req.method;
+  const path = req.path;
+  const ip = req.ip;
+  console.log(`${method} ${path} - ${ip}`);
+  next();
+});
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
